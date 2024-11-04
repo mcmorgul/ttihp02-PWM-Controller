bf960cec4ba0ef22cb51d97f8bfba8d9d5e34f4f
M Ceylan Morgul <49791617+mcmorgul@users.noreply.github.com>
2024-11-04 15:47:19 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,37 +4,63 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles
+from cocotb.binary import BinaryValue
 
 @cocotb.test()
-async def test_project(dut):
+async def test_pwm_with_reset_and_timing(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
+    dut._log.info("PWM")
     dut.ena.value = 1
-    dut.ui_in.value = 0
+    dut.ui_in.value = 0b0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    a = 0
+    for i in range(10):
+        await ClockCycles(dut.clk, 1)
+        if(dut.uo_out[0].value == 1):
+            a = a + 1
+    assert a == 5
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+        
+
+    dut.ui_in[0].value = 1
+
+    b = 0
+    for i in range(10):
+        await ClockCycles(dut.clk, 1)
+        dut.ui_in[0].value = 0
+        if(dut.uo_out[0].value == 1):
+            b = b + 1
+    assert b == 6
+
+
+    await ClockCycles(dut.clk, 10)
+
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 10)
+
+    dut.ui_in[1].value = 1
+
+    c = 0
+    for i in range(10):
+        await ClockCycles(dut.clk, 1)
+        dut.ui_in[1].value = 0
+        if(dut.uo_out[0].value == 1):
+            c = c + 1
+    assert c == 4
+
+
+            
+
